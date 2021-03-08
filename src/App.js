1f8b8186e5c59ee79703750300952efc704c4e4d
F5362849 <ntsikelelo.madikane@fnb.co.za>
2021-03-08 17:45:04 +0200

    NavBar Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
-  <h2>Hello Ntsikelelo</h2>
+  <Navbar/>  
+
   );
 }
 
